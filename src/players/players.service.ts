7c7d0956b0c9221060557eea033fcfadba3196ea
Dionatan <dionatan.1751@gmail.com>
2023-06-12 14:07:14 -0300

    get e delete controller

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CreatePlayerDto } from './dtos/create-player.dto';
 import { Player } from './interfaces/player.interface';
 import { v4 as uuidv4 } from 'uuid';
@@ -10,8 +10,34 @@ export class PlayersService {
   private readonly logger = new Logger(PlayersService.name);
 
   async createUpdatePlayer(createPlayerDto: CreatePlayerDto): Promise<void> {
-    this.logger.log(`createPlayerDto: ${createPlayerDto}`);
+    const { email } = createPlayerDto;
+
+    const playerFound = this.players.find((player) => player.email === email);
+
+    if (playerFound) {
+      return this.update(playerFound, createPlayerDto);
+    }
     await this.create(createPlayerDto);
+  }
+
+  async getAllPlayers(): Promise<Player[]> {
+    return this.players;
+  }
+
+  async getPlayerByEmail(email: string): Promise<Player> {
+    const playerFound = this.players.find((player) => player.email === email);
+    if (!playerFound) {
+      throw new NotFoundException(`Player with email ${email} not found`);
+    }
+    return playerFound;
+  }
+
+  async deletePlayer(email: string): Promise<void> {
+    const playerFound = this.players.find((player) => player.email === email);
+
+    this.players = this.players.filter(
+      (player) => player.email !== playerFound.email,
+    );
   }
 
   private async create(createPlayerDto: CreatePlayerDto): Promise<void> {
@@ -26,6 +52,13 @@ export class PlayersService {
       rankingPosition: 1,
       urlAvatar: `www.google.com.gr/foto123.jpg`,
     };
+    this.logger.log(`createPlayerDto: ${JSON.stringify(player)}`);
     this.players.push(player);
+  }
+
+  private update(playerFound: Player, createPlayerDto: CreatePlayerDto): void {
+    const { name } = createPlayerDto;
+
+    playerFound.name = name;
   }
 }
