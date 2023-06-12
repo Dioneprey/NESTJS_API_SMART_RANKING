08cfdf9b047e49410dbcb26f490b5be74fd427f0
Dionatan <dionatanonliveira@cidadaoon.com>
2023-06-12 13:58:52 -0300

    mongo db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PlayersModule } from './players/players.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://dione:2Kw418YdXOTmMBVL@nest-api-db.vqfmdcm.mongodb.net/smartranking',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
+    PlayersModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/players/interfaces/player.schema.ts
@@ -0,0 +1,13 @@
+import * as mongoose from 'mongoose';
+
+export const PlayerSchema = new mongoose.Schema(
+  {
+    phone: { type: String, unique: true },
+    email: { type: String, unique: true },
+    name: { type: String },
+    ranking: String,
+    rankingPosition: Number,
+    urlAvatar: String,
+  },
+  { timestamps: true, collection: 'players' },
+);
